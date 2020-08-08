555e46d2b97d1b58a14b2f0e96fbc1e0a8ee9696
MrMeta <bungyo92@gmail.com>
2020-08-08 22:37:56 +0900

    Fix author info

--- config.js
+++ config.js
@@ -25,9 +25,9 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Meta Park',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'I want to acquire metacognitive ability',
     contacts: {
       email: '',
       facebook: '#',
